c39dbfd2681204002038e45eb3efc37acb8f0aee
sigfridGom <sigfridgom@gmail.com>
2017-06-28 13:22:48 -0600

    sexto vergazo

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@
 
 mvn clean install
 mvn cobertura:cobertura com.gavinmogan:codacy-maven-plugin:coverage -Dcobertura.report.format=xml -DcoverageReportFile=target/site/cobertura/coverage.xml -DprojectToken=22083380204b4f679a2234a4c4a07fb0 -DapiToken=22083380204b4f679a2234a4c4a07fb0
-
+asd
