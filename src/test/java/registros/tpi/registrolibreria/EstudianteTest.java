0d5934865bbe7f26017105dff6d41f68697aa79b
sigfridGom <sigfridgom@gmail.com>
2017-06-28 13:33:27 -0600

    septimo

---
+++ src/test/java/registros/tpi/registrolibreria/EstudianteTest.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registros.tpi.registrolibreria;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sigfrid
+ */
+public class EstudianteTest {
+    
+    public EstudianteTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getCarnet method, of class Estudiante.
+     */
+    @Test
+    public void testGetCarnet() {
+        System.out.println("getCarnet");
+        Estudiante instance = new Estudiante();
+        String expResult = "";
+        String result = instance.getCarnet();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCarnet method, of class Estudiante.
+     */
+    @Test
+    public void testSetCarnet() {
+        System.out.println("setCarnet");
+        String carnet = "";
+        Estudiante instance = new Estudiante();
+        instance.setCarnet(carnet);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCarrera method, of class Estudiante.
+     */
+    @Test
+    public void testGetCarrera() {
+        System.out.println("getCarrera");
+        Estudiante instance = new Estudiante();
+        int expResult = 0;
+        int result = instance.getCarrera();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCarrera method, of class Estudiante.
+     */
+    @Test
+    public void testSetCarrera() {
+        System.out.println("setCarrera");
+        int carrera = 0;
+        Estudiante instance = new Estudiante();
+        instance.setCarrera(carrera);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDui method, of class Estudiante.
+     */
+    @Test
+    public void testGetDui() {
+        System.out.println("getDui");
+        Estudiante instance = new Estudiante();
+        Persona expResult = null;
+        Persona result = instance.getDui();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDui method, of class Estudiante.
+     */
+    @Test
+    public void testSetDui() {
+        System.out.println("setDui");
+        Persona dui = null;
+        Estudiante instance = new Estudiante();
+        instance.setDui(dui);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of hashCode method, of class Estudiante.
+     */
+    @Test
+    public void testHashCode() {
+        System.out.println("hashCode");
+        Estudiante instance = new Estudiante();
+        int expResult = 0;
+        int result = instance.hashCode();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of equals method, of class Estudiante.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("equals");
+        Object object = null;
+        Estudiante instance = new Estudiante();
+        boolean expResult = false;
+        boolean result = instance.equals(object);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Estudiante.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Estudiante instance = new Estudiante();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
